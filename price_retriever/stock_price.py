98c8c03c81c6da00e4916d30276363279bb74f5f
Federico Gobea <fgobea@flopsolutions.com>
2021-04-12 22:22:19 -0300

    [ADD] price_retriever: stock_closes are now stored in attribute
    Also prints stock price instance with ticker, periods and prices

--- price_retriever/stock_price.py
+++ price_retriever/stock_price.py
@@ -2,8 +2,19 @@
 import pandas_datareader.data as web
 
 class StockPrice:
+
   ticker: str
   daily_close_prices: list[float]
-  def __init__(self:StockPrice, ticker: str) -> None:
+
+  def _stock_closes(self: StockPrice) -> list[float]:
+    stock_info = web.DataReader(self.ticker,'stooq')
+    return stock_info['Close'].to_list()
+
+  def __init__(self: StockPrice, ticker: str) -> None:
     self.ticker = ticker
-    
+    self.daily_close_prices = self._stock_closes()
+
+  def __repr__(self: StockPrice) -> str:
+    prices = self.daily_close_prices
+    return 'StockPrice({ticker} | {periods} Periodos | Precios: {prices})'.format(ticker=self.ticker, periods=len(prices), prices=','.join(prices))
+
