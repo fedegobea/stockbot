5132d180c438c9f0e905838fb5db3fcef97dae03
Federico Gobea <fgobea@flopsolutions.com>
2021-04-12 21:49:32 -0300

    [ADD] stock_cli: added first functions for cli (WIP)

---
+++ stock_cli/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from .price import get_stock_price
+
+
+print('Hola')
+input('Que precio de accion queres conseguir?')
+
+
---
+++ stock_cli/price.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def get_stock_price(ticker=False):
+  if not ticker:
+    print('No se envio un ticker')
