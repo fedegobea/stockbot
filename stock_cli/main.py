5bad84a155dd4505ed9af2c07e83d4374d17a868
Mariano Bujia <mariano.bujia@gmail.com>
2021-04-12 23:26:58 -0300

    Agregue periods

--- stock_cli/main.py
+++ stock_cli/main.py
@@ -3,7 +3,10 @@ from .price import get_stock_price
 
 
 print('Hola')
-ticker = input('El precio de que accion queres obtener?')
+ticker = input('¿El precio de que accion queres obtener?')
+periods = input('¿Que periodos quisera ver?')
 print(get_stock_price(ticker))
+
+
 
 
