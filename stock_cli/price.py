a084928d20026c0d407722ce3fa1eb1e031c4eb3
Mariano Bujia <mariano.bujia@gmail.com>
2021-04-12 23:26:58 -0300

    Agregue periods

--- stock_cli/price.py
+++ stock_cli/price.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from ..price_retriever.stock_price import StockPrice
 
-def get_stock_price(ticker=False):
+def get_stock_price(ticker=False, periods=20):
   if not ticker:
     print('No se envio un ticker')
-  stock_price = StockPrice(ticker)
+  stock_price = StockPrice(ticker, periods)
   return stock_price
